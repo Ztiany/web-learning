eae5438d0a2e358494d4420edf35ded085f68fb2
ztiany <ztiany3@gmail.com>
2023-09-24 23:05:12 +0800

    update vue

---
+++ 04-vue/imooc-vue3-recruitment-client/src/utils/request.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import {showToast} from "vant";
+
+let baseURL = '/api';
+const service = axios.create({
+    baseURL,
+    timeout: 10000
+})
+
+// 请求拦截器
+service.interceptors.request.use(
+    config => {
+        const token = window.localStorage.getItem('token');
+        if (token) {
+            config.params = {'token': token}
+        }
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+)
+
+// 相应拦截器
+service.interceptors.response.use(
+    response => {
+        const res = response.data
+        if (response.status !== 200) {
+            return Promise.reject(new Error(res.success || 'Error'))
+        } else {
+            if (res.code == 200) {
+                return res.result || res.data
+            } else {
+                showToast(res.success)
+            }
+        }
+    },
+    error => {
+        return Promise.reject(error)
+    }
+)
+
+export default service;
---
+++ 04-vue/imooc-vue3-recruitment-client/vite.config.ts
@@ -0,0 +1,25 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {fileURLToPath, URL} from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    //因为存在跨域请求，所以需要配置 proxy。
+    server: {
+        port: 8002,
+        host: '0.0.0.0',
+        open: true,
+        proxy: {
+            '/api/upload': 'https://mobile.zcwytd.com',
+            '/api': 'https://api.imooc.zcwytd.com'
+        },
+        cors: true
+    },
+    //路径解析
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        }
+    }
+})
